57cc69474abbf477fe232e550baedf251a97d70b
Erikolssonr <Erikolssonr@gmail.com>
2024-02-07 15:15:23 +0100

    checkout fixes
    asdasd

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -10,8 +10,9 @@ const Checkout = () => {
   const [address, setAddress] = useState("");
   const [creditCardNumber, setCreditCardNumber] = useState("");
   const [creditCardOwner, setCreditCardOwner] = useState("");
-  const [creditCardDate, setCreditCardDate] = useState("");
-  const [creditCardCVC, setCreditCardCVC] = useState("");
+  const [creditCardDateMonth, setCreditCardDateMonth] = useState("");
+  const [creditCardDateYear, setCreditCardDateYear] = useState("");
+  const [creditCardCVV, setCreditCardCVV] = useState("");
   const [postalCode, setPostalCode] = useState("");
 
   const handleFirstNameChange = (e) => {
@@ -36,11 +37,14 @@ const Checkout = () => {
   const handleCreditCardOwner = (e) => {
     setCreditCardOwner(e.target.value);
   };
-  const handleCreditCardDate = (e) => {
-    setCreditCardDate(e.target.value);
+  const handleCreditCardDateMonth = (e) => {
+    setCreditCardDateMonth(e.target.value);
   };
-  const handleCreditCardCVC = (e) => {
-    setCreditCardCVC(e.target.value);
+  const handleCreditCardDateYear = (e) => {
+    setCreditCardDateMonth(e.target.value);
+  };
+  const handleCreditCardCVV = (e) => {
+    setCreditCardCVV(e.target.value);
   };
   const handlePostalCode = (e) => {
     setPostalCode(e.target.value);
@@ -55,164 +59,183 @@ const Checkout = () => {
       <div className="flex">
         <div className="flex-1 container mt-8 ml-20">
           <h1 className="text-3xl font-poppins mb-4">Checkout</h1>
-          <form onSubmit={handleSubmit} className="max-w-md">
-            <div>
-            <div className="mb-4">
-              <label
-                htmlFor="firstName"
-                className="block text-sm font-poppins text-gray-600"
-              >
-                Förnamn
-              </label>
-              <input
-                type="text"
-                id="firstName"
-                value={firstName}
-                onChange={handleFirstNameChange}
-                className="mt-1 p-2 border rounded-md w-full"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="lastName"
-                className="block text-sm font-poppins text-gray-600"
-              >
-                Efternamn
-              </label>
-              <input
-                type="text"
-                id="lastName"
-                value={lastName}
-                onChange={handleLastNameChange}
-                className="mt-1 p-2 border rounded-md w-full"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block text-sm font-poppins text-gray-600"
-              >
-                E-postadress
-              </label>
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={handleEmailChange}
-                className="mt-1 p-2 border rounded-md w-full"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="address"
-                className="block text-sm font-poppins text-gray-600"
-              >
-                Adress
-              </label>
-              <input
-                type="text"
-                id="address"
-                value={address}
-                onChange={handleAddress}
-                className="mt-1 p-2 border rounded-md w-full"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="postalCode"
-                className="block text-sm font-poppins text-gray-600"
-              >
-                Postnummer
-              </label>
-              <input
-                type="text"
-                id="postalCode"
-                value={postalCode}
-                onChange={handlePostalCode}
-                className="mt-1 p-2 border rounded-md w-full"
-                required
-              />
-            </div>
-            </div>
-            <div className="">
-            <div className="mb-4">
-              <label
-                htmlFor="creditCardNumber"
-                className="block text-sm font-poppins text-gray-600"
-              >
-                Kortnummer
-              </label>
-              <input
-                type="text"
-                id="creditCardNumber"
-                value={creditCardNumber}
-                onChange={handleCreditCardNumber}
-                className="mt-1 p-2 border rounded-md w-full"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="creditCardOwner"
-                className="block text-sm font-poppins text-gray-600"
-              >
-                Kortinnehavare
-              </label>
-              <input
-                type="text"
-                id="creditCardOwner"
-                value={creditCardOwner}
-                onChange={handleCreditCardOwner}
-                className="mt-1 p-2 border rounded-md w-full"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="creditCardDate"
-                className="block text-sm font-poppins text-gray-600"
-              >
-                Utgångsdatum
-              </label>
-              <input
-                type="text"
-                id="creditCardDate"
-                value={creditCardDate}
-                onChange={handleCreditCardDate}
-                className="mt-1 p-2 border rounded-md w-full"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="creditCardCVC"
-                className="block text-sm font-poppins text-gray-600"
-              >
-                CVC
-              </label>
-              <input  
-                type="text"
-                id="creditCardCVC"
-                value={creditCardCVC}
-                onChange={handleCreditCardCVC}
-                className="mt-1 p-2 border rounded-md w-full"
-                required
-              />
-            </div>
-            </div>
-            
-          </form>
-
-          {}
+            <form onSubmit={handleSubmit} className="max-w-md">
+              <div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="firstName"
+                    className="block text-sm font-poppins text-gray-600"
+                  >
+                    Förnamn
+                  </label>
+                  <input
+                    type="text"
+                    id="firstName"
+                    value={firstName}
+                    onChange={handleFirstNameChange}
+                    className="mt-1 p-2 border rounded-md w-full"
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="lastName"
+                    className="block text-sm font-poppins text-gray-600"
+                  >
+                    Efternamn
+                  </label>
+                  <input
+                    type="text"
+                    id="lastName"
+                    value={lastName}
+                    onChange={handleLastNameChange}
+                    className="mt-1 p-2 border rounded-md w-full"
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-poppins text-gray-600"
+                  >
+                    E-postadress
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="example@example.com"
+                    id="email"
+                    value={email}
+                    onChange={handleEmailChange}
+                    className="mt-1 p-2 border rounded-md w-full"
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="address"
+                    className="block text-sm font-poppins text-gray-600"
+                  >
+                    Adress
+                  </label>
+                  <input
+                    type="text"
+                    id="address"
+                    value={address}
+                    onChange={handleAddress}
+                    className="mt-1 p-2 border rounded-md w-full"
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="postalCode"
+                    className="block text-sm font-poppins text-gray-600"
+                  >
+                    Postnummer
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="1234 56"
+                    maxLength="7"
+                    id="postalCode"
+                    value={postalCode}
+                    onChange={handlePostalCode}
+                    className="mt-1 p-2 border rounded-md w-full"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="pt-10">                                 
+                <div className="mb-4">
+                  <label
+                    htmlFor="creditCardNumber"
+                    className="block text-sm font-poppins text-gray-600"
+                  >
+                    Kortnummer
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="1234 5678 9123 4567"
+                    maxlength="19"
+                    id="creditCardNumber"
+                    value={creditCardNumber}
+                    onChange={handleCreditCardNumber}
+                    className="mt-1 p-2 border rounded-md w-full"
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="creditCardOwner"
+                    className="block text-sm font-poppins text-gray-600"
+                  >
+                    Kortinnehavare
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Förnamn Efternamn"
+                    id="creditCardOwner"
+                    value={creditCardOwner}
+                    onChange={handleCreditCardOwner}
+                    className="mt-1 p-2 border rounded-md w-full"
+                    required
+                  />
+                </div>
+                <div className="flex p-3 space-x-10">
+                  <div className="mb-4">
+                    <div>
+                    <label
+                      htmlFor="creditCardDate"
+                      className="block text-sm font-poppins text-gray-600"
+                    >
+                      Utgångsdatum
+                    </label>
+                      <input
+                        type="text"
+                        placeholder="MM"
+                        id="creditCardDateMonth"
+                        value={creditCardDateMonth}
+                        onChange={handleCreditCardDateMonth}
+                        className="mt-1 p-2 border rounded-md w-full"
+                        required
+                      />
+                    <input 
+                        type="text"
+                        placeholder="YY"
+                        id="creditCardDateYear"
+                        value={creditCardDateYear}
+                        onChange={handleCreditCardDateYear}
+                        className="mt-1 p-2 border rounded-md w-full"
+                        required 
+                      />
+                    </div>
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="creditCardCVC"
+                      className="block text-sm font-poppins text-gray-600"
+                    >
+                      CVV
+                    </label>
+                    <input  
+                      type="text"
+                      maxLength="3"
+                      placeholder="XXX"
+                      id="creditCardCVV"
+                      value={creditCardCVV}
+                      onChange={handleCreditCardCVV}
+                      className="mt-1 p-2 border rounded-md w-full"
+                      required
+                    />
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+            <div className="flex-1">
+              <CheckoutCart />
+          </div>
         </div>
-        <div className="flex-1">
-        <CheckoutCart />
-      </div>
-      </div>
     </>
   );
 };
